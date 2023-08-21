689ff76ebd7a9a9d0e7fe3ed9421e559298d37cf
rachitsgambit <asthanarachit@gmail.com>
2023-08-21 21:24:04 +0530

    Initial Upload

---
+++ algo.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int func(int a,int b){
+    int n= b-a;
+    if(n<=9){
+        for(int i=a;i<b;i++){
+        switch(i){
+            case 0:
+            cout<<"zero"<<endl;
+            break;
+            case 1:
+            cout<<"one"<<endl;
+            break;
+            case 2:
+            cout<<"two"<<endl;
+            break;
+            case 3:
+            cout<<"three"<<endl;
+            break;
+            case 4:
+            cout<<"four"<<endl;
+            break;
+            case 5:
+            cout<<"five"<<endl;
+            break;
+            case 6:
+            cout<<"six"<<endl;
+            break;
+            case 7:
+            cout<<"seven"<<endl;
+            break;
+            case 8:
+            cout<<"eight"<<endl;
+            break;
+            case 9:
+            cout<<"nine"<<endl;
+            break;
+        }
+        }
+    }
+    else{
+        for(int i=10;i<b;i++){
+            if(i%2==0){
+                cout<<"even"<<endl;
+            }
+            else{
+                cout<<"odd"<<endl;
+            }
+        }
+    }
+    return 0;
+}
+int main() {
+    // Complete the code.
+    int n,m;
+    cin>>n>>m;
+    func(n,m);
+    return 0;
+}
