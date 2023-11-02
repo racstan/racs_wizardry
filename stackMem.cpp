594f835f20ee4bf93a95cccd6b0c0aa417777c4a
rachitsgambit <asthanarachit@gmail.com>
2023-11-02 16:27:24 +0530

    Added comment

--- stackMem.cpp
+++ stackMem.cpp
@@ -9,7 +9,9 @@ int main(){
     int mainVariable = 69;
         std::cout << "Inside main: " << mainVariable << std::endl;
         functionOnStack();
-        
+
         std::cout << "Back inside main: " << mainVariable << std::endl;
+    // Attempting to access stackVariable here would result in a compilation error,
+    // as it's out of scope and not accessible in this function.
 
 }
