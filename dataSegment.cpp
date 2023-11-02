06e1e5b2918b51d1736d136a55c7b3314a2261a2
rachitsgambit <asthanarachit@gmail.com>
2023-11-02 18:29:11 +0530

    Update

--- dataSegment.cpp
+++ dataSegment.cpp
@@ -6,4 +6,6 @@ static int staticVariable = 10;
 
 int main(){
     std::cout << "Global variable: " << globalVariable << std::endl;
+    std::cout << "Static variable: " << staticVariable << std::endl;
+
 }
