154f4f046cb9e4fba0bea2956b8e7287470425a8
rachitsgambit <asthanarachit@gmail.com>
2023-11-04 22:47:10 +0530

    Redo

--- ExceptionHandling.cpp
+++ ExceptionHandling.cpp
@@ -1,22 +1,2 @@
 #include <iostream>
-using namespace std;
-
-class exceptions{
-    public:
-        int a, b;
-        void getdata(){
-            cout << "Enter the value of a and b: ";
-            cin >> a >> b;
-        }
-        void divide(){
-            int c;
-            try{
-                if(b==0){
-                    throw b;
-
-                }
-
-            }
-        }
-};
-}
+#include <stdexcept>
