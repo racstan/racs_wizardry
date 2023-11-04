6e63e67e6ba9c6b152e0b47757b48b53fffc8fd9
rachitsgambit <asthanarachit@gmail.com>
2023-11-04 22:31:52 +0530

    Update

--- ExceptionHandling.cpp
+++ ExceptionHandling.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 class exceptions{
     public:
+        int a, b;
+        void getdata(){
+            cout << "Enter the value of a and b: ";
+            cin >> a >> b;
+        }
 };
 }
