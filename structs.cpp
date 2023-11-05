6722a6bb134cb74f152d0a8adf2062cd79ba7226
rachitsgambit <asthanarachit@gmail.com>
2023-11-05 10:57:18 +0530

    Final Commit

--- structs.cpp
+++ structs.cpp
@@ -4,15 +4,15 @@
 struct Person{
     std::string name;
     int age;
-}
+};
 
 int main() {
     Person person1;
-     person1.name = "John";
-     person1.age =20;
+    person1.name = "John";
+    person1.age =20;
 
-     Person person2={"Jane",21};
+    Person person2={"Jane",21};
     std::cout << "Person 1: " << person1.name << ", " << person1.age << " years old." << std::endl;
-        std::cout << "Person 2: " << person2.name << ", " << person2.age << " years old." << std::endl;
+    std::cout << "Person 2: " << person2.name << ", " << person2.age << " years old." << std::endl;
 return 0;
 }
