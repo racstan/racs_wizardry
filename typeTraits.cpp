9124cf0cfaa555c410054e4c2c81c2bc8ec6c5dc
rachitsgambit <asthanarachit@gmail.com>
2023-11-10 21:27:23 +0530

    Update

--- typeTraits.cpp
+++ typeTraits.cpp
@@ -5,6 +5,7 @@ template <typename T>
 void printTypeInfo() {
     std::cout << "Type: " << typeid(T).name() << std::endl;
     if (std::is_integral<T>::value) {
+        std::cout << "This is an integer type." << std::endl;
 
 
 
