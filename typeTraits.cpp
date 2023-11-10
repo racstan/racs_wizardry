ec3f4bee3ee2892c716ae5cab074036e78269173
rachitsgambit <asthanarachit@gmail.com>
2023-11-10 21:27:40 +0530

    Update

--- typeTraits.cpp
+++ typeTraits.cpp
@@ -6,6 +6,7 @@ void printTypeInfo() {
     std::cout << "Type: " << typeid(T).name() << std::endl;
     if (std::is_integral<T>::value) {
         std::cout << "This is an integer type." << std::endl;
+    } else {
 
 
 
