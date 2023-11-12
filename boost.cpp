46e9e0c1d97514595678196d962d0221ba27dcc3
rachitsgambit <asthanarachit@gmail.com>
2023-11-12 10:02:21 +0530

    Update

--- boost.cpp
+++ boost.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
-#include <vector>
-
-class ResourceManager {
-public:
-    ResourceManager() {
-        std::cout << "Resource acquired." << std::endl;
-}
-
-    ~ResourceManager() {
-        std::cout << "Resource released." << std::endl;
- }
-
-    void performTask() {
-
-
+#include <boost/version.hpp>
 
 
 
