d15a958938fdb0b444597d2f0bc98cd93b73b176
rachitsgambit <asthanarachit@gmail.com>
2023-11-14 22:17:39 +0530

    Update

--- greeter_client.cpp
+++ greeter_client.cpp
@@ -37,6 +37,7 @@ int main() {
   GreeterClient greeter(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
   std::string user("world");
   std::string reply = greeter.SayHello(user);
+  std::cout << "Greeter received: " << reply << std::endl;
 
 
 
