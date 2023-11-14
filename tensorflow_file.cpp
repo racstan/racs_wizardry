3c1aa82c924f5b8b37fb20e32a2703227ec0d5fe
rachitsgambit <asthanarachit@gmail.com>
2023-11-14 23:04:48 +0530

    Update

--- tensorflow_file.cpp
+++ tensorflow_file.cpp
@@ -10,6 +10,7 @@ int main() {
 
     if (TF_GetCode(status) != TF_OK) {
         std::cerr << "Error initializing TensorFlow: " << TF_Message(status) << std::endl;
+        TF_DeleteStatus(status);
 
 
 
