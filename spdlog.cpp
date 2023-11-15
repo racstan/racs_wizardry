9be26960ab74eacf7748066b043a9d7058122a45
rachitsgambit <asthanarachit@gmail.com>
2023-11-15 20:21:35 +0530

    Update

--- spdlog.cpp
+++ spdlog.cpp
@@ -6,6 +6,7 @@ int main() {
     auto console_logger = spdlog::stdout_color_mt("console");
     auto file_logger = spdlog::basic_logger_mt("file_logger", "example.log");
 
+    console_logger->info("This is an informational message.");
 
 
 
