feaa078feac94ace1b69b2a0cc09e45f35e8f4b0
rachitsgambit <asthanarachit@gmail.com>
2023-11-16 21:08:03 +0530

    Update

--- fmt.cpp
+++ fmt.cpp
@@ -7,7 +7,6 @@ int main() {
     int x = 5;
     double y = 3.14;
     std::string name = "John";
-
     std::cout << fmt::format("Values: {}, {}, {}", x, y, name) << std::endl;
     std::cout << fmt::sprintf("The answer is %.2f", 42.0) << std::endl;
     std::string result = fmt::format("The result is {}", 123);
