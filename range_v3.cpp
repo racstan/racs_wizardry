570cdd5908a66ae0f8433c647d08b1f648305b2e
rachitsgambit <asthanarachit@gmail.com>
2023-11-17 22:05:58 +0530

    Update

--- range_v3.cpp
+++ range_v3.cpp
@@ -6,6 +6,7 @@ int main() {
 
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     auto even_numbers = numbers | ranges::view::filter([](int x) { return x % 2 == 0; });
+    auto squared_numbers = numbers | ranges::view::transform([](int x) { return x * x; });
 
 
 
