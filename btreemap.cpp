69238974ee1fee875b77b4d31e1db862b201d5c7
rachitsgambit <asthanarachit@gmail.com>
2023-11-19 22:35:22 +0530

    update

--- btreemap.cpp
+++ btreemap.cpp
@@ -6,6 +6,7 @@ fn main() {
     btree_map.insert(3, String::from("Three"));
     btree_map.insert(1, String::from("One"));
     btree_map.insert(4, String::from("Four"));
+    btree_map.insert(2, String::from("Two"));
 
 
 
