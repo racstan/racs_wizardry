a49912f418935fdf00b8d5b7bfdeedb7bda47ec1
rachitsgambit <asthanarachit@gmail.com>
2023-11-25 23:31:18 +0530

    update

--- rwlock.rs
+++ rwlock.rs
@@ -1,7 +1,7 @@
 use std::sync::{RwLock, Arc};
 use std::thread;       
 struct SharedData {     
-    data: Vec<i32>, 
+    data: Vec<i32>,  
     rw_lock: RwLock<Vec<i32>>,
 }
 fn read_data(shared_data: Arc<SharedData>) {
