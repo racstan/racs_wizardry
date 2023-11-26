d564548ced1e8a101090ac0528fb1cb6020cbf07
rachitsgambit <asthanarachit@gmail.com>
2023-11-26 23:04:47 +0530

    update

--- arc.rs
+++ arc.rs
@@ -3,7 +3,7 @@ use std::thread;
       
 struct SharedData {
     counter: Arc<Mutex<u32>>,
-}
+} 
 fn increment_counter(shared_data: Arc<Mutex<u32>>, thread_id: u32) {
     let mut counter = shared_data.lock().unwrap();
     *counter += 1;
