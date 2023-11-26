df8f9efff69ebebcaadd8b7a19616b149c9f1f07
rachitsgambit <asthanarachit@gmail.com>
2023-11-26 23:05:08 +0530

    update

--- arc.rs
+++ arc.rs
@@ -8,7 +8,7 @@ fn increment_counter(shared_data: Arc<Mutex<u32>>, thread_id: u32) {
     let mut counter = shared_data.lock().unwrap();
     *counter += 1;
     println!("Thread {}: Counter: {}", thread_id, *counter);
-} 
+}  
 fn main() {
     let shared_data = Arc::new(Mutex::new(0));
     let thread1_data = Arc::clone(&shared_data);
