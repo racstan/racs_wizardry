3fd0679394e5ed26942cfbc9426966bd1c5fefb7
rachitsgambit <asthanarachit@gmail.com>
2023-11-27 22:21:44 +0530

    update

--- tokio.rs
+++ tokio.rs
@@ -2,7 +2,7 @@ use tokio::time::{sleep, Duration};
 async fn async_task(id: u32) {     
     println!("Task {} started", id); 
     sleep(Duration::from_secs(2)).await; 
-    println!("Task {} completed", id);
+    println!("Task {} completed", id); 
 }
 async fn main() {
     let task1 = async_task(1);
