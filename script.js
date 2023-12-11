2ae042886820ea3881a56d093e309e1522ee3bd7
racstan <80248074+racstan@users.noreply.github.com>
2023-12-11 22:30:00 +0530

    update

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let first = document.getElementById("firstNum");
 let second = document.getElementById("secondNum");
 function add(){
     let result =parseInt(first.value) + parseInt(second.value);
+    document.getElementById("result").innerHTML = result;
