47e6e0ff8afa5eb55ec1708aa1358740583111f2
racstan <asthanarachit@gmail.com>
2024-02-28 22:27:51 +0530

    Update

--- kidsgreatestcandies.cpp
+++ kidsgreatestcandies.cpp
@@ -12,12 +12,23 @@ class Solution{
             if((candy+extraCandies)==max){
                 results.push_back(true);
             }
+            else{
+                results.push_back(false);
+            }
         }
         return results;
     }
 };
 int main(){
+    vector<int> candies = {2,3,4,5,6};
+    int extraCandies = 3;
 
+    Solution solution;
+    vector<bool> result = solution.kidsWithCandies(candies, extraCandies);
+    for(bool canHaveCandies:result){
+        cout<<(canHaveCandies ? "true": "false")<<" ";
+    }
+    cout<<endl;
     return 0;
 
 }
