6d74a9d42a3871c0a0d7d248466f52a01f1f826e
racstan <asthanarachit@gmail.com>
2024-02-29 21:50:31 +0530

    update

--- canPlaceFlowers.cpp
+++ canPlaceFlowers.cpp
@@ -7,7 +7,7 @@ class Solution{
     public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int empty = 0;
-        for(int i=0;i<flowerbed.size();i=i+2){
+        for(int i=0;i<flowerbed.size();i=i+1){
             if(i<flowerbed.size()-2) {
                 if (flowerbed[i] == 1) {
                     if (flowerbed[i + 2] == 0 &&flowerbed[i+1]==0) {
